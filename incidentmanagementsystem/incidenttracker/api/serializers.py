1fee2d92e932c5e1c08f8634fa74a20358891a52
Hemant Dagar <hemant.dagar00@gmail.com>
2022-07-28 04:34:51 -0400

    Completed

---
+++ incidentmanagementsystem/incidenttracker/api/serializers.py
@@ -0,0 +1,98 @@
+from urllib import request
+from random import randint
+from django.contrib.auth import get_user_model, models
+from django.contrib.auth.hashers import make_password
+from django.db import transaction
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+import re
+from datetime import datetime
+from incidentmanagementsystem.incidenttracker.models import IncidentData, IncidentSearch
+from incidentmanagementsystem.users.api.serializers import UserSerializer
+
+User = get_user_model()
+
+
+class IncidentDetailSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source="reporter_name.username")
+
+    class Meta:
+        model = IncidentData
+        fields = ["id", "incident_number", "user", "detail", "priority", "status", "reported_date_time", "url"]
+
+        extra_kwargs = {
+            "url": {"view_name": "api:incident-detail", "lookup_field": "id"}
+        }
+
+
+class IncidentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IncidentData
+        fields = ["incident_number", "reporter_name", "detail", "priority", "reported_date_time",]
+        read_only_fields = ("incident_number", "reporter_name", "reported_date_time",)
+
+    def save(self, validated_data):
+        detail = validated_data.pop('detail')
+        priority = validated_data.pop('priority')
+
+        now = datetime.now()
+        year = now.year
+        i=0
+        while i==0:
+            inc_no = randint(10000, 99999)
+            incident_number = "RMG" + str(inc_no) + str(year)
+            if IncidentData.objects.filter(incident_number=incident_number).exists():
+                pass
+            else:
+                i=1
+
+        instance = IncidentData.objects.create(
+            incident_number=incident_number,
+            reporter_name=self.context['request'].user,
+            detail=detail,
+            reported_date_time=now,
+            priority=priority,
+            # status = ,
+        )
+        return instance
+
+
+class IncidentUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IncidentData
+        fields = ["detail", "priority", "status" ]
+
+    def validate(self, attrs):
+        if self.instance.reporter_name != self.context['request'].user:
+            raise ValidationError("You are not allowed to update the incident reported by other user.")
+        elif self.instance.status == "Closed":
+            raise ValidationError("You are not allowed to update the incident with 'Closed' status.")
+        return attrs
+
+    def update(self, instance, validated_data):
+        instance.detail = validated_data.get('detail', instance.detail)
+        instance.priority = validated_data.get('priority', instance.priority)
+        instance.status = validated_data.get('status', instance.status)
+        instance.save()
+        return instance
+
+
+
+# class IncidentSearchCreateSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = IncidentSearch
+#         fields = ["search_incident"]
+#
+#     def save(self, validated_data):
+#         search_incident = validated_data.pop('search_incident')
+#
+#         if not IncidentData.objects.filter(incident_number=search_incident,reporter_name=self.context['request'].user).exists():
+#             raise ValidationError("This incident is not available.")
+#
+#         IncidentSearch.objects.all().delete()
+#
+#         instance = IncidentSearch.objects.create(
+#             search_incident=search_incident,
+#             user=self.context['request'].user,
+#         )
+#         return instance
---
+++ incidentmanagementsystem/incidenttracker/api/views.py
@@ -0,0 +1,109 @@
+from attr import attrs
+from django.contrib.auth import get_user_model
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status, generics
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin, DestroyModelMixin
+from rest_framework.exceptions import NotFound
+from .serializers import IncidentDetailSerializer, IncidentCreateSerializer, IncidentUpdateSerializer
+# from .serializers import IncidentSearchCreateSerializer
+from incidentmanagementsystem.incidenttracker.models import IncidentData, IncidentSearch
+
+from rest_framework import filters
+
+User = get_user_model()
+
+class IncidentViewSet(RetrieveModelMixin,
+                  ListModelMixin,
+                  UpdateModelMixin,
+                  DestroyModelMixin,
+                  CreateModelMixin,
+                  GenericViewSet):
+
+    queryset = IncidentData.objects.all()
+    lookup_field = "id"
+
+    search_fields = ['incident_number']
+    filter_backends = (filters.SearchFilter,)
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return IncidentCreateSerializer
+
+        elif self.request.method == 'PUT':
+            return IncidentUpdateSerializer
+        else:
+            return IncidentDetailSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        obj = serializer.save(serializer.validated_data)
+        response_serializer = IncidentDetailSerializer(obj, context={'request': request})
+        return Response({"response": response_serializer.data, "status": "success"}, status=status.HTTP_201_CREATED)
+
+    def get_queryset(self, *args, **kwargs):  # used in get_object
+        return self.queryset.filter(reporter_name=self.request.user)
+
+
+    def get_object(self, *args, **kwargs):  # used in update
+        self.queryset = self.get_queryset()
+        obj = self.queryset.filter(id=self.kwargs['id']).first()
+        if not obj:
+            raise NotFound("Incident not found.")
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        data_to_change = {'detail': request.data.get("detail"),
+                          'priority': request.data.get("priority"),
+                          'status': request.data.get("status"),
+                          }
+        obj = self.get_object()
+        serializer = self.get_serializer(obj, data=data_to_change, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({"response": serializer.data, "status": "success"}, status=status.HTTP_202_ACCEPTED)
+
+
+# class IncidentSearchViewSet(RetrieveModelMixin,
+#                   ListModelMixin,
+#                   UpdateModelMixin,
+#                   DestroyModelMixin,
+#                   CreateModelMixin,
+#                   GenericViewSet):
+#
+#     queryset = IncidentData.objects.all()
+#     lookup_field = "id"
+#
+#     def get_serializer_class(self):
+#         if self.request.method == 'POST':
+#             return IncidentSearchCreateSerializer
+#         else:
+#             return IncidentDetailSerializer
+#
+#     def get_queryset(self, *args, **kwargs):  # used in get_object
+#         incident_number = IncidentSearch.objects.filter(user=self.request.user).values("search_incident")
+#         if len(incident_number)==0:
+#             return self.queryset.filter(reporter_name=self.request.user,incident_number="00")
+#         else:
+#             return self.queryset.filter(reporter_name=self.request.user,incident_number=incident_number[0]['search_incident'])
+#
+#
+#     def get_object(self, *args, **kwargs):  # used in update
+#         self.queryset = self.get_queryset()
+#         obj = self.queryset.filter(id=self.kwargs['id']).first()
+#         if not obj:
+#             raise NotFound("Incident not found.")
+#         return obj
+#
+#     def create(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         obj = serializer.save(serializer.validated_data)
+#         response_serializer = IncidentSearchCreateSerializer(obj, context={'request': request})
+#         return Response({"response": response_serializer.data, "status": "success"}, status=status.HTTP_201_CREATED)
+#
